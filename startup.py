94bd06bf436dbeea8bd70d93d68f7d5abd3594e5
cho45 <cho45@lowreal.net>
2022-12-12 23:10:39 +0900

    update

---
+++ startup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+
+device = sh1106(i2c(port=1, address=0x3C))
+device.persist = True
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="black", fill="black")
+    draw.text((10, 16), text="Starting...", fill="white")
+
