edeb62c652191ed1687a336e57af0f26811908c4
cho45 <cho45@lowreal.net>
2022-12-14 23:20:28 +0900

    update

--- shutdown.py
+++ shutdown.py
@@ -2,9 +2,9 @@
 
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
-from luma.oled.device import sh1106
+from luma.oled.device import ssd1306
 
-device = sh1106(i2c(port=1, address=0x3C))
+device = ssd1306(i2c(port=1, address=0x3C))
 device.persist = True
 
 with canvas(device) as draw:
