ff70bb93c13a0e84c6c1e811ac3e38103215c616
cho45 <cho45@lowreal.net>
2022-12-16 16:42:33 +0900

    update

---
+++ reset.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+
+device = ssd1306(i2c(port=1, address=0x3C))
+device.persist = True
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="black", fill="black")
+    draw.text((10, 16), text="TEST", fill="white")
+
