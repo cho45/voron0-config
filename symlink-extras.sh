c6eb232234c2dab05747bbd9b8362630764036e0
cho45 <cho45@lowreal.net>
2022-12-17 15:42:06 +0900

    update

--- symlink-extras.sh
+++ symlink-extras.sh
@@ -6,7 +6,6 @@ KLIPPER_PATH="${HOME}/klipper"
 SRCDIR="$HOME//klipper_config"
 
 ln -sf "${SRCDIR}/network_status.py" "${KLIPPER_PATH}/klippy/extras/network_status.py"
-ln -sf "${SRCDIR}/led_effect.py" "${KLIPPER_PATH}/klippy/extras/led_effect.py"
 
 sudo ln -sf "${SRCDIR}/99-i2c.rules" "/etc/udev/rules.d/99-i2c.rules"
 sudo ln -sf ~/klipper_config/powerdown.py /usr/lib/systemd/system-shutdown/
